2e36297b7e64964283b5e6a31f7e01f430b8662a
atsuchak <suchak0630@gmail.com>
2026-02-16 22:09:32 +0600

    feat: add Firebase configuration to `config.js` and update `.gitignore` to track it.

---
+++ config.js
@@ -0,0 +1,11 @@
+// IMPORTANT: This file is ignored by git.
+// DO NOT commit this file to GitHub.
+// Update the apiKey below with your NEW regenerated key from Google Cloud Console.
+const firebaseConfig = {
+    apiKey: "AIzaSyCby41xaHE4PRctNKNV8kB4paeCfRwQgB8",
+    authDomain: "tasked-todo-v1-app.web.app",
+    projectId: "tasked-todo-v1-app",
+    storageBucket: "tasked-todo-v1-app.firebasestorage.app",
+    messagingSenderId: "939919890388",
+    appId: "1:939919890388:web:d93ced08bf26259a94e841"
+};
